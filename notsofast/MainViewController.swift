63e8d96615e8cd04237254dbb6c468f8e1ad80c6
Yuri Karabatov <yuri@yurikarabatov.com>
2018-05-08 17:52:18 +0300

    Rename ViewController to MainViewController

---
+++ notsofast/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  notsofast
+//
+//  Created by Yuri Karabatov on 28/04/2018.
+//  Copyright © 2018 Yuri Karabatov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+}
+
