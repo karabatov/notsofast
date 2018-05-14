ff8e2c97e24f71dc209e1242eaea72b22b4338dc
Yuri Karabatov <yuri@yurikarabatov.com>
2018-05-14 21:49:33 +0300

    No need for red color anymore

--- notsofast/AppDelegate.swift
+++ notsofast/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let mainVC = MainViewController()
-        mainVC.view.backgroundColor = UIColor.red
 
         if window == nil {
             window = UIWindow(frame: UIScreen.main.bounds)
