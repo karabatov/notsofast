4c3887eaecad26b4da4f3065db525aa2aef93b2b
Yuri Karabatov <yuri@yurikarabatov.com>
2018-05-15 22:47:28 +0300

    Add Constants class

---
+++ notsofast/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  notsofast
+//
+//  Created by Yuri Karabatov on 15/05/2018.
+//  Copyright © 2018 Yuri Karabatov. All rights reserved.
+//
+
+import Foundation
+
+/// Abstract class to store constants for the running app.
+final class Constants {
+    private init() {}
+}
