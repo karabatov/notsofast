b0e4d2ba0276ad5af091743443c0319f4b391294
Yuri Karabatov <yuri@yurikarabatov.com>
2018-05-17 13:03:00 +0300

    Add MealWheelModel

---
+++ notsofast/model/MealWheelModel.swift
@@ -0,0 +1,13 @@
+//
+//  MealWheelModel.swift
+//  notsofast
+//
+//  Created by Yuri Karabatov on 17/05/2018.
+//  Copyright © 2018 Yuri Karabatov. All rights reserved.
+//
+
+import Foundation
+
+final class MealWheelModel {
+
+}
