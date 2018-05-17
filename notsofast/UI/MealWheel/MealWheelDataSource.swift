0d7c9e31b4d6d650460226c325527b7656fd635a
Yuri Karabatov <yuri@yurikarabatov.com>
2018-05-17 12:45:16 +0300

    Add MealWheelDataSource

---
+++ notsofast/UI/MealWheel/MealWheelDataSource.swift
@@ -0,0 +1,20 @@
+//
+//  MealWheelDataSource.swift
+//  notsofast
+//
+//  Created by Yuri Karabatov on 17/05/2018.
+//  Copyright © 2018 Yuri Karabatov. All rights reserved.
+//
+
+import UIKit
+
+/// Connects the meal wheel model to the actual collection view.
+final class MealWheelDataSource: NSObject, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
