3be8dded90c8d1cfaf029e10d061f35fbfa27e05
Yuri Karabatov <yuri@yurikarabatov.com>
2018-05-19 10:40:20 +0300

    Add MainViewModel

---
+++ notsofast/model/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  notsofast
+//
+//  Created by Yuri Karabatov on 19/05/2018.
+//  Copyright © 2018 Yuri Karabatov. All rights reserved.
+//
+
+import Foundation
+
+final class MainViewModel {
+}
