c5eb0bfa6f497f51831ca3e472a60759d3d83c8f
Yuri Karabatov <yuri@yurikarabatov.com>
2018-06-26 22:21:29 +0300

    Start making a view model

--- notsofast/model/EditMealViewModel.swift
+++ notsofast/model/EditMealViewModel.swift
@@ -7,8 +7,25 @@
 //
 
 import Foundation
+import RxSwift
+
+struct EditMealSection {
+    let title: String?
+    let rows: [EditMealCell]
+}
+
+enum EditMealCell {
+    case size(Serving)
+    case ingredients(Nutrients)
+    case date(Date)
+    case delete
+}
 
 /// View model for the create/edit meal view controller.
 final class EditMealViewModel {
+    let data = ReplaySubject<[EditMealSection]>.create(bufferSize: 1)
 
+    required init() {
+        data.onNext([])
+    }
 }
