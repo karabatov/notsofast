310db022c6d5bd8c1596011206bbbd5c803f18f2
Yuri Karabatov <yuri@yurikarabatov.com>
2018-06-27 23:18:39 +0300

    Create a MealActionController

--- notsofast/helpers/CoreDataProvider.swift
+++ notsofast/helpers/CoreDataProvider.swift
@@ -15,8 +15,13 @@ enum FetchResultsTarget {
     case twentyFourHourList
 }
 
+protocol MealActionController {
+    func upsert(meal: Meal)
+    func delete(meal: Meal)
+}
+
 /// Maintains a Core Data stack and returns preconfigured results controllers.
-final class CoreDataProvider {
+final class CoreDataProvider: MealActionController {
     static var sharedInstance = CoreDataProvider()
 
     private let container: NSPersistentContainer
@@ -45,5 +50,51 @@ final class CoreDataProvider {
             try? frc.performFetch()
             return frc
         }
+    }
+
+    // MARK: MealActionController
+
+    func upsert(meal: Meal, original: Meal) {
+        container.performBackgroundTask { context in
+            let fr = NSFetchRequest<MealEntity>(entityName: "MealEntity")
+            fr.predicate = NSPredicate(format: "eaten = %@", argumentArray: [original.eaten])
+
+            guard let mealsCount = try? context.count(for: fr) else { return }
+
+            switch mealsCount {
+            case 0:
+                let newMealEntity = MealEntity(context: context)
+                newMealEntity.eaten = meal.eaten
+                newMealEntity.what = meal.what
+                newMealEntity.size = Int32(meal.size.rawValue)
+                newMealEntity.nutri = Int64(meal.nutri.rawValue)
+
+            case 1:
+                if
+                    let meals = try? context.fetch(fr),
+                    meals.count == 1,
+                    let firstMeal = meals.first
+                {
+                    firstMeal.eaten = meal.eaten
+                    firstMeal.what = meal.what
+                    firstMeal.size = Int32(meal.size.rawValue)
+                    firstMeal.nutri = Int64(meal.nutri.rawValue)
+                }
+
+            default:
+                return
+            }
+
+
+            do {
+                try context.save()
+            } catch {
+                NSFLog("Context failed to save: \(meal)")
+            }
+        }
+    }
+
+    func delete(meal: Meal) {
+        NSFLog("Meal deletion requested.")
     }
 }
