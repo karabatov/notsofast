d8c2efc9195d87a6b5f76f5c7b56c52bb2d9fef5
Yuri Karabatov <yuri@yurikarabatov.com>
2018-08-06 20:15:11 +0300

    Display left and right arrow buttons on the nav bar

--- notsofast/MealListViewController.swift
+++ notsofast/MealListViewController.swift
@@ -10,8 +10,34 @@ import UIKit
 
 /// Display a list of meals in a collection view.
 final class MealListViewController: UIViewController {
+    private let leftButton = UIBarButtonItem(image: R.image.arrow_left(), style: UIBarButtonItemStyle.plain, target: self, action: #selector(MealListViewController.leftButtonPressed))
+    private let rightButton = UIBarButtonItem(image: R.image.arrow_right(), style: UIBarButtonItemStyle.plain, target: self, action: #selector(MealListViewController.rightButtonPressed))
+
+    // MARK: System methods
+
+    required init() {
+        super.init(nibName: nil, bundle: nil)
+
+        navigationItem.hidesBackButton = true
+        navigationItem.leftBarButtonItem = leftButton
+        navigationItem.rightBarButtonItem = rightButton
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) not implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    // MARK: Navbar items
+
+    @objc func leftButtonPressed() {
+        NSFLog("Left pressed")
+    }
+
+    @objc func rightButtonPressed() {
+        NSFLog("Left pressed")
     }
 }
