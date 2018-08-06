658f4be19b3e1fb5bcfbee90ec719f469c4c6b22
Yuri Karabatov <yuri@yurikarabatov.com>
2018-08-06 19:56:37 +0300

    Add a collection view based controller for meals

---
+++ notsofast/MealListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MealListViewController.swift
+//  notsofast
+//
+//  Created by Yuri Karabatov on 06/08/2018.
+//  Copyright © 2018 Yuri Karabatov. All rights reserved.
+//
+
+import UIKit
+
+/// Display a list of meals in a collection view.
+final class MealListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
