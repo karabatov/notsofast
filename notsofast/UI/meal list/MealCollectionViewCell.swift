4028a7091d995abbc4ad34c1ceebf17eb5463246
Yuri Karabatov <yuri@yurikarabatov.com>
2018-08-08 23:10:41 +0300

    Start setting up MealCollectionViewCell

--- notsofast/UI/meal list/MealCollectionViewCell.swift
+++ notsofast/UI/meal list/MealCollectionViewCell.swift
@@ -10,18 +10,68 @@ import UIKit
 
 final class MealCollectionViewCell: UICollectionViewCell {
     static let reuseIdentifier = "MealCollectionViewCell"
+    private let servingLabel = UILabel(frame: CGRect.zero)
+    private let absoluteDateLabel = UILabel(frame: CGRect.zero)
+    private let relativeDateLabel = UILabel(frame: CGRect.zero)
+    private let proteinView = UIView(frame: CGRect.zero)
+    private let fastCarbView = UIView(frame: CGRect.zero)
+    private let slowCarbView = UIView(frame: CGRect.zero)
+    private let fatView = UIView(frame: CGRect.zero)
+    private let nutriContainer = UIStackView(frame: CGRect.zero)
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        backgroundColor = UIColor.red
+        commonInit()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) not implemented")
     }
 
+    private func commonInit() {
+        backgroundColor = UIColor.mealListCellBackground
+
+        servingLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(servingLabel)
+
+        relativeDateLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(relativeDateLabel)
+
+        absoluteDateLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(absoluteDateLabel)
+
+        nutriContainer.axis = .horizontal
+        nutriContainer.alignment = .fill
+        nutriContainer.spacing = 0.0
+        nutriContainer.distribution = .fillEqually
+        nutriContainer.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(nutriContainer)
+
+        nutriContainer.addArrangedSubview(proteinView)
+        nutriContainer.addArrangedSubview(fastCarbView)
+        nutriContainer.addArrangedSubview(slowCarbView)
+        nutriContainer.addArrangedSubview(fatView)
+
+
+    }
+
     func configure(model: MealCellModel) {
-        //
+        servingLabel.text = model.size
+        absoluteDateLabel.text = model.absoluteDate
+        relativeDateLabel.attributedText = model.relativeDate
+
+        func colorView(view: UIView, nutri: Nutrients, color: UIColor) {
+            if model.nutrients.contains(nutri) {
+                view.backgroundColor = color
+            } else {
+                view.backgroundColor = UIColor.clear
+            }
+        }
+
+        colorView(view: proteinView, nutri: Nutrients.protein, color: UIColor.protein)
+        colorView(view: fastCarbView, nutri: Nutrients.fastCarb, color: UIColor.fastCarb)
+        colorView(view: slowCarbView, nutri: Nutrients.slowCarb, color: UIColor.slowCarb)
+        colorView(view: fatView, nutri: Nutrients.fat, color: UIColor.fat)
     }
 }
--- notsofast/extensions/UIColorPreset.swift
+++ notsofast/extensions/UIColorPreset.swift
@@ -9,5 +9,17 @@
 import UIKit
 
 extension UIColor {
+    /// Global app tint color.
     static let nsfTintColor = UIColor.red
+    /// Solid background for rounded cells in the meal list collection view.
+    static let mealListCellBackground = UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)
+
+    /// Protein color.
+    static let protein = UIColor(red: 148.0/255.0, green: 226.0/255.0, blue: 250.0/255.0, alpha: 1.0)
+    /// Fast carb color.
+    static let fastCarb = UIColor(red: 248.0/255.0, green: 148.0/255.0, blue: 250.0/255.0, alpha: 1.0)
+    /// Slow carb color.
+    static let slowCarb = UIColor(red: 181.0/255.0, green: 245.0/255.0, blue: 195.0/255.0, alpha: 1.0)
+    /// Fat color.
+    static let fat = UIColor(red: 230.0/255.0, green: 237.0/255.0, blue: 148.0/255.0, alpha: 1.0)
 }
