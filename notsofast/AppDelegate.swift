7a9713af1b6a95253f02aaa6d5752fcb15553a5d
Yuri Karabatov <yuri@yurikarabatov.com>
2018-08-10 18:58:44 +0300

    Semiautoupdating data config

--- notsofast/AppDelegate.swift
+++ notsofast/AppDelegate.swift
@@ -7,17 +7,20 @@
 //
 
 import UIKit
+import RxSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    private var mealDataProvider: MealListDataProvider?
+    private var timerDisposeBag = DisposeBag()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let config = MealListDataConfig(startDate: Date.init(timeIntervalSinceNow: -60.0 * 60.0 * 24.0), endDate: Date.distantFuture)
         let dp = CoreDataProvider.sharedInstance.dataProviderForMealList(config: config)
+        mealDataProvider = dp
         let vm = MealListViewModel(dataProvider: dp)
         let mainVC = MealListViewController(dataSource: vm, viewModel: vm)
         let nav = UINavigationController(rootViewController: mainVC)
@@ -29,12 +32,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
 
+        setupDataSourceTimer()
+
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        timerDisposeBag = DisposeBag()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -48,12 +54,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        setupDataSourceTimer()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    // MARK: Helpers
 
+    private func setupDataSourceTimer() {
+        guard let dp = mealDataProvider else { return }
+
+        timerDisposeBag = DisposeBag()
+
+        dp.dataConfig
+            .sample(Observable<Int>.timer(1.0, period: 5.0, scheduler: MainScheduler.asyncInstance))
+            .debug("DATACONFIG")
+            .map { dataConfig -> MealListDataConfig in
+                return dataConfig
+            }
+            .bind(to: dp.dataConfig)
+            .disposed(by: timerDisposeBag)
+    }
 }
 
