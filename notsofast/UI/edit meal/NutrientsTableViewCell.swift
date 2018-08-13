abb206719f2aa22c22a01a6dedaa2f19ed65e02a
Yuri Karabatov <yuri@yurikarabatov.com>
2018-08-13 21:31:04 +0300

    Add NutrientsTableViewCell

---
+++ notsofast/UI/edit meal/NutrientsTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  NutrientsTableViewCell.swift
+//  notsofast
+//
+//  Created by Yuri Karabatov on 13/08/2018.
+//  Copyright © 2018 Yuri Karabatov. All rights reserved.
+//
+
+import UIKit
+
+final class NutrientsTableViewCell: UITableViewCell {
+    static let reuseIdentifier = "NutrientsTableViewCell"
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) not implemented")
+    }
+}
